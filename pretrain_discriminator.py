8642f89155459f59bdc4967cc5f2bcfa4d6be155
Andrew Bailey <bailey.andrew4@gmail.com>
2017-12-04 19:09:50 -0800

    fix bug with single word tweets

--- pretrain_discriminator.py
+++ pretrain_discriminator.py
@@ -351,21 +351,26 @@ class TrainingData(object):
                 self.tweet_queue.put(tweet)
                 if self.dis_pretrain:
                     fake_tweet = self.create_fake_tweet(tweet)
-                    self.fake_tweet_queue.put(fake_tweet)
+                    if fake_tweet:
+                        self.fake_tweet_queue.put(fake_tweet)
 
         np.random.shuffle(self.all_tweets)
 
     def create_fake_tweet(self, tweet):
         """Swap words in tweet"""
         words = tweet.split()
-        n_swaps = int((len(words) * self.swap_prob)/2)
-        # print(len(words), prob, n_swaps)
-        index = np.random.choice(range(0, len(words)-1, 2), n_swaps, replace=False)
-        for i in index:
-            tmp_word = words[i]
-            words[i] = words[i+1]
-            words[i+1] = tmp_word
-        return ' '.join(words)
+        if len(words) > 1:
+            n_swaps = int((len(words) * self.swap_prob)/2)
+            # print(len(words), prob, n_swaps)
+
+            index = np.random.choice(range(0, len(words)-1, 2), n_swaps, replace=False)
+            for i in index:
+                tmp_word = words[i]
+                words[i] = words[i+1]
+                words[i+1] = tmp_word
+            return ' '.join(words)
+        else:
+            return False
 
     def start_threads(self, n_threads=1):
         """ Start background threads to feed queue """
@@ -420,99 +425,110 @@ def main():
     len_x, max_seq_len, ix_to_char, char_to_ix, all_tweets, all_seq_len = load_tweet_data([file_list[1]],
                                                                                           reduction_level=reduction_level)
 
+    tweet = "aas"
+    words = tweet.split()
+    n_swaps = int((len(words) * self.swap_prob)/2)
+    # print(len(words), prob, n_swaps)
+    if len(words) > 1:
+        index = np.random.choice(range(0, len(words)-1, 2), n_swaps, replace=False)
+        for i in index:
+            tmp_word = words[i]
+            words[i] = words[i+1]
+            words[i+1] = tmp_word
+        print(' '.join(words))
 
-    len_y = 1
-    # create placeholders for discriminator
-    place_X = tf.placeholder(tf.float32, shape=[None, max_seq_len, len_x], name='Input')
-    place_Y = tf.placeholder(tf.float32, shape=[None, len_y], name='Label')
-    place_Seq = tf.placeholder(tf.int32, shape=[None], name='Sequence_Length')
-    # define discriminator and generator global steps
-    d_global_step = tf.get_variable(
-        'd_global_step', [],
-        initializer=tf.constant_initializer(0), trainable=False)
-
-    # create easily accessible training data
-    training_data = TrainingData(all_tweets, all_seq_len, len_x, batch_size, char_to_ix=char_to_ix,
-                                 end_tweet_char=end_tweet_char, gen_pretrain=False, dis_pretrain=True)
-    training_data.start_threads(n_threads=threads)
-    # training_data.stop_threads()
-    # x,s,y = training_data.get_batch()
-    # print(y.shape)
-    # discriminator for twitter data
-    Dx = pretrain_discriminator(place_X, place_Seq, d_n_hidden, len_y, forget_bias=forget_bias, reuse=False)
-    log.info("Discriminator Model Built")
-
-    # discriminator accuracy
-    d_accuracy = tf.reduce_mean(tf.cast(tf.equal(Dx, place_Y), tf.float32))
-    # calculate loss
-    d_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=Dx, labels=place_Y))
-    log.info("Created Loss function")
-
-    # create summary info
-    variable_summaries(d_loss, "discriminator_loss")
-    variable_summaries(d_accuracy, "discriminator_accuracy")
-    all_summary = tf.summary.merge_all()
-
-    # partition trainable variables
-    tvars = tf.trainable_variables()
-    d_vars = [var for var in tvars if 'pretrain_d_lstm' in var.name]
-
-    # define optimizers
-    opt = tf.train.AdamOptimizer(learning_rate=learning_rate)
-
-    # define update step
-    trainerD = opt.minimize(d_loss, var_list=d_vars, global_step=d_global_step)
-    log.info("Defined Optimizers")
-
-    # define config
-    config = tf.ConfigProto(log_device_placement=False,
-                            intra_op_parallelism_threads=8,
-                            allow_soft_placement=True)
-
-    with tf.Session(config=config) as sess:
-        if load_model:
-            writer = tf.summary.FileWriter(trained_model_dir, sess.graph)
-            saver = tf.train.Saver(max_to_keep=4, keep_checkpoint_every_n_hours=2)
-            saver.restore(sess, model_path)
-            write_graph = True
-            log.info("Loaded Model: {}".format(trained_model_dir))
-        else:
-            # initialize
-            writer = tf.summary.FileWriter(output_dir, sess.graph)
-            sess.run(tf.global_variables_initializer())
-            saver = tf.train.Saver(max_to_keep=4, keep_checkpoint_every_n_hours=2)
-            saver.save(sess, model_path,
-                       global_step=d_global_step)
-            write_graph = True
-
-        # start training
-        log.info("Started Training")
-        step = 0
-        while step < iterations:
-            x_batch, seq_batch, y_batch = training_data.get_batch()
-            # z_batch = np.random.normal(0, 1, size=[batch_size, len_x])
-            _, dLoss, dAccuracy= sess.run([trainerD, d_loss, d_accuracy], feed_dict={place_X: x_batch,
-                                                               place_Seq: seq_batch,
-                                                               place_Y: y_batch})
-            # print(dPredict)
-            # print(dAccuracy)
-            # print(dLoss)
-            # z_batch = np.random.normal(0, 1, size=[batch_size, len_x])
-            step += 1
-            if step % 100 == 0:
-                summary_info, d_step = sess.run([all_summary, d_global_step], feed_dict={place_X: x_batch,
-                                                                                         place_Seq: seq_batch,
-                                                                                         place_Y: y_batch})
-                print("Step {}: Discriminator Loss {}".format(d_step, dLoss))
-                writer.add_summary(summary_info, global_step=d_step)
-                saver.save(sess, model_path,
-                           global_step=d_global_step, write_meta_graph=write_graph)
-                write_graph = False
-
-    training_data.stop_threads()
-    log.info("Finished Training")
-
-
-
-if __name__ == '__main__':
-    main()
+#     len_y = 1
+#     # create placeholders for discriminator
+#     place_X = tf.placeholder(tf.float32, shape=[None, max_seq_len, len_x], name='Input')
+#     place_Y = tf.placeholder(tf.float32, shape=[None, len_y], name='Label')
+#     place_Seq = tf.placeholder(tf.int32, shape=[None], name='Sequence_Length')
+#     # define discriminator and generator global steps
+#     d_global_step = tf.get_variable(
+#         'd_global_step', [],
+#         initializer=tf.constant_initializer(0), trainable=False)
+#
+#     # create easily accessible training data
+#     training_data = TrainingData(all_tweets, all_seq_len, len_x, batch_size, char_to_ix=char_to_ix,
+#                                  end_tweet_char=end_tweet_char, gen_pretrain=False, dis_pretrain=True)
+#     training_data.start_threads(n_threads=threads)
+#     # training_data.stop_threads()
+#     # x,s,y = training_data.get_batch()
+#     # print(y.shape)
+#     # discriminator for twitter data
+#     Dx = pretrain_discriminator(place_X, place_Seq, d_n_hidden, len_y, forget_bias=forget_bias, reuse=False)
+#     log.info("Discriminator Model Built")
+#
+#     # discriminator accuracy
+#     d_accuracy = tf.reduce_mean(tf.cast(tf.equal(Dx, place_Y), tf.float32))
+#     # calculate loss
+#     d_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=Dx, labels=place_Y))
+#     log.info("Created Loss function")
+#
+#     # create summary info
+#     variable_summaries(d_loss, "discriminator_loss")
+#     variable_summaries(d_accuracy, "discriminator_accuracy")
+#     all_summary = tf.summary.merge_all()
+#
+#     # partition trainable variables
+#     tvars = tf.trainable_variables()
+#     d_vars = [var for var in tvars if 'pretrain_d_lstm' in var.name]
+#
+#     # define optimizers
+#     opt = tf.train.AdamOptimizer(learning_rate=learning_rate)
+#
+#     # define update step
+#     trainerD = opt.minimize(d_loss, var_list=d_vars, global_step=d_global_step)
+#     log.info("Defined Optimizers")
+#
+#     # define config
+#     config = tf.ConfigProto(log_device_placement=False,
+#                             intra_op_parallelism_threads=8,
+#                             allow_soft_placement=True)
+#
+#     with tf.Session(config=config) as sess:
+#         if load_model:
+#             writer = tf.summary.FileWriter(trained_model_dir, sess.graph)
+#             saver = tf.train.Saver(max_to_keep=4, keep_checkpoint_every_n_hours=2)
+#             saver.restore(sess, model_path)
+#             write_graph = True
+#             log.info("Loaded Model: {}".format(trained_model_dir))
+#         else:
+#             # initialize
+#             writer = tf.summary.FileWriter(output_dir, sess.graph)
+#             sess.run(tf.global_variables_initializer())
+#             saver = tf.train.Saver(max_to_keep=4, keep_checkpoint_every_n_hours=2)
+#             saver.save(sess, model_path,
+#                        global_step=d_global_step)
+#             write_graph = True
+#
+#         # start training
+#         log.info("Started Training")
+#         step = 0
+#         while step < iterations:
+#             x_batch, seq_batch, y_batch = training_data.get_batch()
+#             # z_batch = np.random.normal(0, 1, size=[batch_size, len_x])
+#             _, dLoss, dAccuracy= sess.run([trainerD, d_loss, d_accuracy], feed_dict={place_X: x_batch,
+#                                                                place_Seq: seq_batch,
+#                                                                place_Y: y_batch})
+#             # print(dPredict)
+#             # print(dAccuracy)
+#             # print(dLoss)
+#             # z_batch = np.random.normal(0, 1, size=[batch_size, len_x])
+#             step += 1
+#             if step % 100 == 0:
+#                 summary_info, d_step = sess.run([all_summary, d_global_step], feed_dict={place_X: x_batch,
+#                                                                                          place_Seq: seq_batch,
+#                                                                                          place_Y: y_batch})
+#                 print("Step {}: Discriminator Loss {}".format(d_step, dLoss))
+#                 writer.add_summary(summary_info, global_step=d_step)
+#                 saver.save(sess, model_path,
+#                            global_step=d_global_step, write_meta_graph=write_graph)
+#                 write_graph = False
+#
+#     training_data.stop_threads()
+#     log.info("Finished Training")
+#
+#
+#
+# if __name__ == '__main__':
+#     main()
